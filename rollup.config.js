5b1f50d8ff954b1ee1ea748a47575b2b0477a027
Jason Miller <developit@users.noreply.github.com>
2021-09-16 11:34:50 -0400

    Simplify package by removing unnecessary syntax variants. (#8)

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import simpleTS from './lib/simple-ts';
 import del from 'del';
 import { terser } from 'rollup-plugin-terser';
-import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 
 function removeDefs() {
   return {
@@ -13,80 +12,31 @@ function removeDefs() {
   };
 }
 
-const babelPreset = [['@babel/preset-env', { targets: { ie: '10' } }]];
-
-function getBabelPlugin() {
-  return getBabelOutputPlugin({
-    presets: babelPreset,
-    allowAllFormats: true,
-  });
-}
-
 export default async function () {
   await del('dist');
 
-  return [
-    // Main builds
-    {
-      input: 'src/index.ts',
-      plugins: [simpleTS('./')],
-      output: [
-        {
-          file: 'dist/esm/index.js',
-          format: 'es',
-        },
-        {
-          file: 'dist/cjs/index.cjs',
-          format: 'cjs',
-          exports: 'default',
-        },
-        {
-          file: 'dist/iife/index-min.js',
-          format: 'iife',
-          name: 'idbReady',
-          esModule: false,
-          plugins: [
-            terser({
-              compress: { ecma: 2020 },
-            }),
-            removeDefs(),
-          ],
-        },
-      ],
-    },
-    // Compat builds
-    {
-      input: 'src/index.ts',
-      external: (id) => {
-        if (id.startsWith('@babel/runtime')) return true;
+  return {
+    input: 'src/index.ts',
+    plugins: [simpleTS('./'), terser()],
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'es',
       },
-      plugins: [simpleTS('./', { noBuild: true })],
-      output: [
-        {
-          file: 'dist/esm-compat/index.js',
-          format: 'es',
-          plugins: [getBabelPlugin()],
-        },
-        {
-          file: 'dist/cjs-compat/index.cjs',
-          format: 'cjs',
-          exports: 'default',
-          plugins: [getBabelPlugin()],
-        },
-        {
-          file: 'dist/iife-compat/index-min.js',
-          format: 'iife',
-          name: 'idbReady',
-          esModule: false,
-          plugins: [
-            getBabelPlugin(),
-            terser({
-              compress: { ecma: 5 },
-            }),
-            removeDefs(),
-          ],
-        },
-      ],
-    },
-  ];
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+        strict: false,
+        exports: 'default',
+        plugins: [removeDefs()],
+      },
+      {
+        file: 'dist/iife.min.js',
+        format: 'iife',
+        name: 'idbReady',
+        esModule: false,
+        plugins: [removeDefs()],
+      },
+    ],
+  };
 }
